a4ed36b23736bb4f19c8e58b5d36eb4b7a759e8b
Yuut <ingride.reis.b@gmail.com>
2022-09-22 22:35:32 -0300

    test: Testando parcialmente o service

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -1,22 +1,40 @@
-// template para criação dos testes de cobertura da camada de service
+ import * as sinon from 'sinon';
+ import chai from 'chai';
+import ModelCar from '../../../models/model.car';
+import ServicesCar from '../../../services/service.car';
+import { idMockModelCar, mockModelCar, mockModelCarError } from '../../mocks';
+ const { expect } = chai;
+ import { ZodError } from 'zod';
 
-
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
-
-// describe('Sua descrição', () => {
-
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
-
-//   after(()=>{
-//     sinon.restore();
-//   })
-
-//   it('', async () => {});
-
-// });
+describe('Cars Service', () => {
+    const carsModel = new ModelCar ();
+    const carsService = new ServicesCar(carsModel);
+  
+    describe('Create new Car', () => {
+      before(async () => {
+        sinon
+          .stub(carsModel, 'create')
+          .resolves(idMockModelCar);
+  
+      });
+  
+      after(() => {
+        sinon.restore();
+      })
+      it('Success', async () => {
+        const carCreated = await carsService.create(mockModelCar);
+        expect(carCreated).to.be.deep.equal(idMockModelCar)
+      });
+  
+      it('Failed', async () => {
+        let error;
+        try {
+          await carsService.create(mockModelCarError);
+        } catch (err) {
+          error = err
+        }
+        expect(error).to.be.instanceOf(ZodError);
+      })
+    })
+  
+  });
